0610d89c3b6f6fa0c52983593709a76fa293e314
Terbal <joelmoyo249@gmail.com>
2025-10-31 15:50:11 +0100

    update socket et var d'env add

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,5 +1,9 @@
-// src/socket.js
+// frontend/src/socket.js
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000"); // connexion unique
+// Lisez la variable VITE_API_URL que vous venez de définir
+// process.env.VITE_API_URL sera automatiquement injecté par Vite lors du build.
+const VITE_API_URL = import.meta.env.VITE_API_URL; 
+const socket = io(VITE_API_URL); 
+
 export default socket;
